15e8d485782bf3db2dbb6fbda29910853ff7746e
Matt Thompson <mt8093@rit.edu>
2024-02-06 14:53:27 -0500

    Add server crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::{command, Parser};
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String
+}
+
+
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
 }
